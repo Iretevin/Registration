8b4e5af415e10d727bbb0150caf64f579431c616
Irete Vincent <centmuede@gmail.com>
2023-03-20 04:55:30 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Condition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //conditionals
+            Console.WriteLine("please enter your temperature");
+            String temperature = Console.ReadLine();
+            int numTemp = int.Parse(temperature);
+
+            
+            if (numTemp < 20)
+            {
+                Console.WriteLine("Take your coat");
+            }
+
+            if (numTemp == 20)
+            {
+                Console.WriteLine("pull over is good");
+            }
+
+            if (numTemp > 20)
+            {
+                Console.WriteLine("shorts are good");
+            }
+            Console.ReadKey();
+        }
+    }
+}
